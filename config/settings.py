92de3a1df350a8c614e86235e48ee7370bea82b6
Argus817 <namantandel@gmail.com>
2026-05-14 02:51:29 +0530

    Minor changes to settings. Expense add/update done. Timestamp remaining

--- config/settings.py
+++ config/settings.py
@@ -116,18 +116,30 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'mysql.connector.django',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),  
+#         'PORT': os.getenv('DB_PORT'),
+#         'OPTIONS': {
+#             'autocommit': True,
+#             'use_pure': True,
+#         },
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),  
         'PORT': os.getenv('DB_PORT'),
-        'OPTIONS': {
-            'autocommit': True,
-            'use_pure': True,
-        },
+        
     }
 }
 
